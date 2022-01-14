5bc6e06e47425a4ae471468273bdb926d054d446
awpadilha.dev@gmail.com <awpadilha.dev@gmail.com>
2022-01-13 21:55:04 -0300

    Exercicios de fixação funcoes

---
+++ 4 - Javascript + Lógica de programação/22 - funcaoAnonima.js
@@ -0,0 +1,9 @@
+let doubleSpeed = function(velocity){
+    return velocity * 2
+}
+
+let newVelocity = doubleSpeed(40)
+
+console.log(newVelocity)
+
+//funções anonimas por serem atribuidas em variaveis seguem a regra das chamadas de variaveis, por isso ela não pode ser acessada antes da inicialização
---
+++ 4 - Javascript + Lógica de programação/23 - arrowFunctions.js
@@ -0,0 +1,14 @@
+// também são funções anônimas
+
+// const doubleVelocity = (velocity) => {
+//     return velocity * 2
+// }
+
+// console.log(doubleVelocity(60))
+
+
+const doubleVelocity = (velocity) => velocity * 2 //versão reduzida com ou sem aprenteses no velocity
+
+console.log(doubleVelocity(60))
+
+//arrow function é uma simplificação de uma função anônima
